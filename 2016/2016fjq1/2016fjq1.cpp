cdb21de2217c9c56118f3c3e730afc4f0deeb0fd
Michael Lee <ckmichael8@gmail.com>
2016-11-22 09:06:01 +0000

    2016fjq1 Implementing ultimate solution

---
+++ 2016/2016fjq1/2016fjq1.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <vector>
+
+// 2016fjq1 Model Answer
+
+bool test(int a,int b, int case1, int case2, int case3, int case4){
+  
+}
+
+bool arrayEqual(char a[], char b[], int n){
+  int i;
+  for (i=0;i<n;i++){
+    if (a[i]!=b[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+int main(){
+  int i;
+  int P,N;
+  scanf("%d %d\n",&P,&N);
+  char a[N];
+  
+  // processing input
+  for (i=0;i<N;i++){
+    *(a+i) = getchar();
+  }
+  char b[N];
+  for (i=0;i<N;i++){
+    *(b+i) = getchar();
+  }
+  char c[N];
+  for (i=0;i<N;i++){
+    *(c+i) = getchar();
+  }
+  
+  // Fulfiling task 1
+  if (P==N){
+    if (arrayEqual(a,b,N) || arrayEqual(a,c,N)){
+      printf("Impossible");
+    }else{
+      for (i=0;i<N;i++){
+        putchar(*(a+i));
+      }
+      printf("\n");
+    }
+    return 0;
+  }
+  
+  // Fulfiling all task
+  
+  /*
+   * case1 --> TFF
+   * case2 --> TTF
+   * case3 --> TFT
+   * case4 --> TTT
+   */
+    vector<int> case1,case2,case3,case4;
+    for (i=0;i<N;i++){
+      if (a[N] == b[N]){
+        if (a[N] == c[N]){
+          case1.push_back(N);
+        }else{
+          case2.push_back(N);
+        }
+      }else{
+        if (a[N] == c[N]){
+          case3.push_back(N);
+        }else{
+          case4.push_back(N);
+        }
+      }
+    }
+}
