c52a21e19b3c4817f75ef7babdc8c307e8592ec0
michaellee8 <ckmichael8@gmail.com>
2016-11-27 09:50:21 +0800

    hkoi_j021

--- judge/hkoi_j021.cpp
+++ judge/hkoi_j021.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
+#include <cstdio>
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <cstring>
 
-vector<long> o_chart
+using namespace std;
 
 class Datestring{
   public:
     string s;
     long h;
-    Datestring(string str, long hash);
+    Datestring(string);
 };
 
 Datestring::Datestring(string str){
   s = str;
-  int y,d;
-  char[] _m;
+  int y,m,d;
+  char _m [1000];
   sscanf(s.c_str(),"%d, %s %d",&d,_m,&y);
+  if (strcmp(_m,"January") == 0){
+	  m = 1;
+  }else if (strcmp(_m,"February") == 0){
+	  m = 2;
+  }else if (strcmp(_m,"March") == 0){
+	  m = 3;
+  }else if (strcmp(_m,"April") == 0){
+	  m = 4;
+  }else if (strcmp(_m,"May") == 0){
+	  m = 5;
+  }else if (strcmp(_m,"June") == 0){
+	  m = 6;
+  }else if (strcmp(_m,"July") == 0){
+	  m = 7;
+  }else if (strcmp(_m,"August") == 0){
+	  m = 8;
+  }else if (strcmp(_m,"September") == 0){
+	  m = 9;
+  }else if (strcmp(_m,"October") == 0){
+	  m = 10;
+  }else if (strcmp(_m,"November") == 0){
+	  m = 11;
+  }else if (strcmp(_m,"December") == 0){
+	  m = 12;
+  }else {
+	  m = 0;
+  }
+  h = d + 32 * m + 400 * y;
 }
 
 int main()
@@ -26,15 +56,17 @@ int main()
   cin >> n;
   int i;
   string s;
-  datestring p;
-  vector<datastring> v;
+  vector<Datestring> v;
   for (i=0;i<n;i++){
     getline(cin,s);
-    v.push_back(s);
+    v.push_back(new DateString(s));
   }
   sort(v.begin(),v.end(),datacompr);
+  for (i=0;i<n;i++){
+	  cout >> v.at(i) >> endl;
+  }
 }
 
-bool datacompr(string s1, string s2){
-  
+bool datacompr(Datestring d1, Datestring d2){
+  return d1.h < d2.h ;
 }
